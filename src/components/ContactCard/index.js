1932555237f37e60ed6ece4d1e2e4ea42f12c99d
Zunaith Shariff <sheriffgaming926@gmail.com>
2024-11-26 00:42:41 +0530

    updates

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -2,7 +2,7 @@ import {ContactName,Profile,Item} from './styledComponents'
 
 const ContactCard = (props) =>{
     const {contactObj,randomColor}= props
-    return(<Item >
+    return(<Item id={`${contactObj.name[0].toUpperCase()}`} >
         <Profile bgColor={randomColor}>
           {contactObj.name[0].toUpperCase()}
         </Profile>
