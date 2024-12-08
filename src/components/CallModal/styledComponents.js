004ab260859ffb67ad2b8158266c16fa7b57c239
Zunaith Shariff <sheriffgaming926@gmail.com>
2024-12-08 10:36:21 +0530

    calling feature implemented

--- src/components/CallModal/styledComponents.js
+++ src/components/CallModal/styledComponents.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ModalContainer = styled.div`
-  height: 100vh;
+  height: 90vh;
   width: 100vw;
 
   background-image: linear-gradient(to top, white, #3f6252);
